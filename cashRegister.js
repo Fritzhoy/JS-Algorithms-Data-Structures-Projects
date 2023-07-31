04f0fc72ac19c3974c755953457bcb0d483c70c3
Camila Marques <82603176+Fritzhoy@users.noreply.github.com>
2023-07-31 05:28:42 -0700

    Update cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -35,3 +35,55 @@ See below for an example of a cash-in-drawer array:
   ["ONE HUNDRED", 100]
 ]
 **/
+
+function checkCashRegister(price, cash, cid) {
+  let changeArr = [];
+  let totalValue = 0;
+  const changeValue = cash - price;
+  let changeControl = changeValue;
+  //copy the cid array into a new array
+  const cidflat = cid.map((item) => item. slice()).reverse();
+  //declear a obj with unit value of each coins and bill in the cashier.
+  const unit = {
+    "ONE HUNDRED": 100,
+    "TWENTY": 20,
+    "TEN": 10,
+    "FIVE": 5,
+    "ONE": 1, 
+    "QUARTER": 0.25,
+    "DIME": 0.1,
+    "NICKEL": 0.05,
+    "PENNY": 0.01
+  };
+  // iterate over the copy cid array to get the change, spreading between coins and bills.
+  for(let value of cidflat){
+    totalValue += value[1];
+    let units = unit[value[0]];
+    //Choose the minor number between the change total unit and value in the cashier units
+    let num = Math.min(Math.floor(changeControl/units), value[1]/units);
+    let change = num*units;
+    //Subtraction from the changeControl, when it is equal to zero means that we already found around change
+    changeControl -= change;
+    //fix 2 decimals for changeControl value
+    changeControl=(changeControl).toFixed(2);
+    value[1] -= change;
+    changeArr.push([value[0], change]);  
+  };
+//total fater spread between bills and coins  
+let valueCashier = changeArr.reduce((total, acc)=>{return total+=acc[1]},0);
+
+//return the change value based in assessment bellow
+return (changeValue > totalValue || changeValue > valueCashier)?{status:"INSUFFICIENT_FUNDS", change: []}:(changeValue == totalValue)?{status: "CLOSED", change: cid}:{status:"OPEN", change: changeArr.filter(element => element[1] > 0)};
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+
+/** 
+Results:
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+Passed:checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+Passed:checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.**/
